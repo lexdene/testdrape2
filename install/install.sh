86c3720818f18042afbc84a2488f6284d8221bf1
lexdene <lexdene@sohu.com>
2013-03-16 15:14:33 +0800

    add install

---
+++ install/install.sh
@@ -0,0 +1,5 @@
+apt-get install -y libapache2-mod-wsgi python-jinja2 python-markdown python-mysqldb
+mysql -u root -p <<EOF
+create database if not exists tp_db;
+grant all privileges on tp_db.* to tp_user@localhost identified by 'tp123321';
+EOF
