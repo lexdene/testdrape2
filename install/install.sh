d4fc91334be48cb2428cb1e19dbfed73c818c848
Elephant_Liu <lexdene@sohu.com>
2013-03-27 13:09:34 +0800

    update install

--- install/install.sh
+++ install/install.sh
@@ -1,4 +1,4 @@
-apt-get install -y libapache2-mod-wsgi python-jinja2 python-markdown python-mysqldb
+apt-get install -y libapache2-mod-wsgi python-jinja2 python-markdown python-mysqldb mysql-server
 mysql -u root -p <<EOF
 create database if not exists tp_db;
 grant all privileges on tp_db.* to tp_user@localhost identified by 'tp123321';
