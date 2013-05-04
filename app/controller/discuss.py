15a048a03196e179c19b35185028a5c2066200c9
Elephant_Liu <lexdene@sohu.com>
2013-05-04 19:07:09 +0800

    tag list in topic list

--- app/controller/discuss.py
+++ app/controller/discuss.py
@@ -28,13 +28,23 @@ class List(frame.DefaultFrame):
 			.join('discuss_reply','last_reply','last_reply.id = tc.last_reply_id') \
 			.join('userinfo','last_reply_ui','last_reply.uid = last_reply_ui.id') \
 			.join('discuss_reply','count_dr','count_dr.tid = dt.id') \
-			.field('COUNT(count_dr.id) as reply_count') \
+			.join('discuss_topic_tag_bridge','ttb','ttb.topic_id = dt.id') \
+			.join('tag','tag','tag.id = ttb.tag_id') \
+			.field('COUNT(DISTINCT count_dr.id) as reply_count') \
+			.field('GROUP_CONCAT(tag.content) as tags') \
 			.order('CASE WHEN last_reply.id is NULL THEN dt.ctime ELSE last_reply.ctime END DESC') \
 			.group('dt.id') \
 			.reflectField(True) \
 			.limit(**aPager.limit()) \
 			.select()
 		
+		# filter tags
+		for topic in aTopicList:
+			if topic['tags'] is None:
+				topic['tag_list'] = list()
+			else:
+				topic['tag_list'] = topic['tags'].split(',')
+
 		self.setVariable('iter',aTopicList)
 		self.setVariable('timestr',app.lib.text.timeStamp2Short)
 		
