863048433211762ad4861ce48936b07739c56b80
Elephant Liu <lexdene@sohu.com>
2013-06-27 13:51:19 +0800

    add pretty page for log

---
+++ app/controller/log.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+'''
+    首页
+    各种动态
+'''
+
+from os import listdir
+from os.path import isfile, join
+import re
+
+from . import frame
+
+DEFAULT_CLS = 'DateList'
+
+
+class DateList(frame.DefaultFrame):
+    '''
+        日志的日期列表
+    '''
+    def process(self):
+        self.setTitle(u'日期列表 - 查看日志')
+
+        log_dir_path = 'data/log'
+        datelist = [f.split('.')[0] for f in listdir(log_dir_path)]
+        datelist.sort(reverse=True)
+        self.setVariable('datelist', datelist)
+
+
+class Content(frame.DefaultFrame):
+    '''
+        日志的内容
+    '''
+    def process(self):
+        self.setTitle(u'日志内容 - 查看日志')
+
+        params = self.params()
+        date = params.get('date', '')
+
+        file_path = 'data/log/%s.log' % date
+        datalist = list()
+        r = re.compile(
+            r'\[(?P<time>[-0-9 :,]*)\] \[(?P<type>[A-Z]*)\] (?P<content>.*)'
+        )
+        with open(file_path, 'r') as f:
+            for line in f:
+                m = r.match(line)
+                datalist.append({
+                    'time': m.group('time'),
+                    'type': m.group('type'),
+                    'content': re.sub(r'\\n(\\n| )*', '\n', m.group('content'))
+                })
+        self.setVariable('datalist', datalist)
