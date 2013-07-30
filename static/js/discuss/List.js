ab357548cfa6bf3662b1b96a813f3013eb661546
Elephant Liu <lexdene@sohu.com>
2013-07-30 19:16:28 +0800

    tag input and tag list
    tag的样式
    submit时输入框内的tag

--- static/js/discuss/List.js
+++ static/js/discuss/List.js
@@ -1,8 +1,9 @@
 (function(jq){
-    var jQuery = undefined;
+	var jQuery = undefined;
 	jq(function(){
 		jq('#post_form').submit(function(){
 			var form = jq(this);
+			submit_tag_input();
 			form.ajaxSubmit({
 				'success':function(){
 					alert('发表成功');
@@ -39,41 +40,49 @@
 				}
 			});
 		});
-	    var tags_input = jq('#tags_input');
-	    tags_input.keyup(function(event){
-		var input = jq(this);
-
-		var val = input.val();
-		var lastcode = val.substr(-1);
-		var trim_val = val.replace(/(^\s*)|(\s*$)/g, "");
-		if( trim_val.length > 0 && ' ' == lastcode ){
-		    console.log(trim_val);
-		    
-		    // remove val
-		    input.val('');
-		    
-		    // create buttons
-		    var button = jq('<span class="tag_item"><span class="tag_val">'+trim_val+'</span><a href="#" class="remove_button">X</a></span>');
-		    button.find('.remove_button').click(function(){
-			jq(this).closest('.tag_item').remove();
-			return false;
-		    });
-		    input.closest('.tag_wrap').find('.tag_list').append(button);
-		    
-		    // set value to real tags
-		    update_value_for_real_tags();
-		    
-		    return false;
-		}
-	    });
-	    function update_value_for_real_tags(){
-		var val_list = [];
-		tags_input.closest('.tag_wrap').find('.tag_list>.tag_item>.tag_val').each(function(){
-		    var sv = jq(this).text();
-		    val_list.push(sv);
+		var tags_input = jq('#tags_input');
+		tags_input.keyup(function(event){
+			var input = jq(this);
+			var val = input.val();
+			var lastcode = val.substr(-1);
+			if( ' ' === lastcode ){
+				submit_tag_input();
+			}
 		});
-		console.log(val_list);
-		jq('#real_tags').val( val_list.join(' ') );
+
+		function submit_tag_input(){
+			var input = tags_input;
+			var val = input.val();
+			var trim_val = val.replace(/(^\s*)|(\s*$)/g, "");
+			if( trim_val.length > 0){
+				console.log(trim_val);
+				
+				// remove val
+				input.val('');
+				
+				// create buttons
+				var button = jq('<span class="tag_item jf_tag"><span class="tag_val">'+trim_val+'</span><a href="#" class="remove_button">x</a></span>');
+				button.find('.remove_button').click(function(){
+					jq(this).closest('.tag_item').remove();
+					return false;
+				});
+				$('#tag_line').append(button);
+				
+				// set value to real tags
+				update_value_for_real_tags();
+				
+				return false;
+			}
+		}
+
+		function update_value_for_real_tags(){
+			var val_list = [];
+			jq('#tag_line').find('.tag_val').each(function(){
+				var sv = jq(this).text();
+				val_list.push(sv);
+			});
+			console.log('val list:', val_list);
+			jq('#real_tags').val( val_list.join(' ') );
 		}
 
 		// focus tag
