95b9764177ee84300e53f96d443cb22959e60481
Elephant Liu <lexdene@sohu.com>
2014-07-04 13:12:47 +0800

    update install.sh to python3

--- install/install.sh
+++ install/install.sh
@@ -1,7 +1,1 @@
-echo $0
-INSTALL_DIR=$(dirname "$0")
-echo $INSTALL_DIR
-
-apt-get install -y apache2 libapache2-mod-wsgi python-jinja2 python-markdown python-mysql.connector mysql-server python-memcache coffeescript node-less ruby-haml node-uglify python-imaging ttf-dejavu-core
-cp $INSTALL_DIR/apache_etc /etc/apache2/sites-available/default
-service apache2 restart
+apt-get install -y apache2 libapache2-mod-wsgi-py3 python3-jinja2 python3-markdown python3-mysql.connector mysql-server python3-memcache coffeescript node-less ruby-haml node-uglify python3-pil ttf-dejavu-core
